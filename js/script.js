0053cf7ac55a622fb617be495ea42a8de533716d
ernestvp <vidaurrer87@gmail.com>
2026-01-27 12:13:28 +0100

    Accesibilidad: añadir skip link todas las páginas

--- js/script.js
+++ js/script.js
@@ -10,3 +10,21 @@ function alternarMenu() {
         nav.style.display = 'flex';
     }
 }
+const modoNormal = document.getElementById("modo-normal");
+const modoContraste = document.getElementById("modo-contraste");
+const modoDislexia = document.getElementById("modo-dislexia");
+
+modoNormal.addEventListener("click", () => {
+  document.body.classList.remove("contraste");
+  document.body.classList.remove("dislexia");
+});
+
+modoContraste.addEventListener("click", () => {
+  document.body.classList.add("contraste");
+  document.body.classList.remove("dislexia");
+});
+
+modoDislexia.addEventListener("click", () => {
+  document.body.classList.add("dislexia");
+  document.body.classList.remove("contraste");
+});
